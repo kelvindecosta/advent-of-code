6c261688a6d0973f5b0efb772b3d1ecbc3ee04e1
Kelvin DeCosta <decostakelvin@gmail.com>
2024-12-24 22:20:07 +0300

    feat(`y2021.d02`): solve puzzle

--- src/y2021/README.md
+++ src/y2021/README.md
@@ -3,3 +3,4 @@
 |      Day       | Challenge                                          | Benchmark (µs) |
 | :------------: | :------------------------------------------------- | -------------: |
 | [01](./d01.rs) | [Sonar Sweep](https://adventofcode.com/2021/day/1) |           5.60 |
+| [02](./d02.rs) | [Dive!](https://adventofcode.com/2021/day/2)       |          12.14 |
---
+++ src/y2021/d02.rs
@@ -0,0 +1,95 @@
+//! # Dive!
+//!
+//! We track the position of the submarine as a `Point`.
+//! Given each `Command`, we simply update the position according to the
+//! instructions.
+
+use crate::util::point::Point;
+
+#[derive(Clone, Copy)]
+pub enum Command {
+  Up(i32),
+  Down(i32),
+  Forward(i32),
+}
+
+pub fn parse(input: &str) -> Vec<Command> {
+  input
+    .trim()
+    .split_ascii_whitespace()
+    .array_chunks::<2>()
+    .map(|[command, value]| {
+      let value = value.parse::<i32>().unwrap();
+      match command {
+        "forward" => Command::Forward(value),
+        "down" => Command::Down(value),
+        "up" => Command::Up(value),
+        _ => unreachable!(),
+      }
+    })
+    .collect()
+}
+
+pub fn p1(input: &[Command]) -> u32 {
+  let final_position =
+    input
+      .iter()
+      .fold(Point::new(0, 0), |pos, &command| match command {
+        Command::Forward(value) => pos + Point::new(value, 0),
+        Command::Down(value) => pos - Point::new(0, value),
+        Command::Up(value) => pos + Point::new(0, value),
+      });
+  (final_position.x * final_position.y).unsigned_abs()
+}
+
+pub fn p2(input: &[Command]) -> u32 {
+  let (final_position, _) =
+    input
+      .iter()
+      .fold(
+        (Point::new(0, 0), 0),
+        |(pos, aim), &command| match command {
+          Command::Down(value) => (pos, aim - value),
+          Command::Up(value) => (pos, aim + value),
+          Command::Forward(value) => {
+            (pos + Point::new(value, aim * value), aim)
+          }
+        },
+      );
+  (final_position.x * final_position.y).unsigned_abs()
+}
+
+#[cfg(test)]
+mod tests {
+  use rstest::rstest;
+
+  use super::*;
+
+  #[rstest]
+  #[case(
+    "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2",
+    150
+  )]
+  fn test_p1(#[case] input: &str, #[case] expected: u32) {
+    assert_eq!(p1(&parse(input)), expected, "input: {input}");
+  }
+
+  #[rstest]
+  #[case(
+    "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2",
+    900
+  )]
+  fn test_p2(#[case] input: &str, #[case] expected: u32) {
+    assert_eq!(p2(&parse(input)), expected, "input: {input}");
+  }
+}
