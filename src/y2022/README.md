a2ee586d9f4d9f2f93e3edb126906ac26d00be1f
Kelvin DeCosta <decostakelvin@gmail.com>
2024-12-26 23:12:52 +0300

    feat(`y2022.d03`): solve puzzle

--- src/y2022/README.md
+++ src/y2022/README.md
@@ -1,6 +1,7 @@
 # Advent of Code [2022](https://adventofcode.com/2022)
 
-|      Day       | Challenge                                                  | Benchmark (µs) |
-| :------------: | :--------------------------------------------------------- | -------------: |
-| [01](./d01.rs) | [Calorie Counting](https://adventofcode.com/2022/day/1)    |           9.61 |
-| [02](./d02.rs) | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) |           10.4 |
+|      Day       | Challenge                                                      | Benchmark (µs) |
+| :------------: | :------------------------------------------------------------- | -------------: |
+| [01](./d01.rs) | [Calorie Counting](https://adventofcode.com/2022/day/1)        |           9.61 |
+| [02](./d02.rs) | [Rock Paper Scissors](https://adventofcode.com/2022/day/2)     |           10.4 |
+| [03](./d03.rs) | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) |          21.27 |
---
+++ src/y2022/d03.rs
@@ -0,0 +1,100 @@
+//! # Rucksack Reorganization
+//!
+//! For part 1, we split each line into its two halves and find the common
+//! letter.
+//!
+//! For part 2, we chunk lines into groups of 3 and then find the common
+//! letter.
+
+pub fn parse(input: &str) -> Vec<&str> {
+  input.trim().lines().collect()
+}
+
+pub fn common(groups: &[&[u8]]) -> u8 {
+  let mut seen = [0; 256];
+
+  for (n, group) in groups.iter().enumerate() {
+    // The last group should simply return the first common item
+    if n == groups.len() - 1 {
+      return group
+        .iter()
+        .find_map(|&b| (seen[b as usize] == groups.len() - 1).then_some(b))
+        .unwrap();
+    }
+
+    // Consider an item to have been seen, only if it is the first
+    // time for that group
+    group.iter().for_each(|&b| {
+      if seen[b as usize] == n {
+        seen[b as usize] += 1;
+      }
+    });
+  }
+
+  unreachable!()
+}
+
+pub fn item_priority(item: u8) -> u32 {
+  u32::from(match item {
+    b'a'..=b'z' => item - b'a' + 1,
+    b'A'..=b'Z' => item - b'A' + 27,
+    _ => unreachable!(),
+  })
+}
+
+pub fn p1(input: &[&str]) -> u32 {
+  input
+    .iter()
+    .map(|line| {
+      let bytes = line.as_bytes();
+      item_priority(common(&[
+        &bytes[0..bytes.len() / 2],
+        &bytes[bytes.len() / 2..],
+      ]))
+    })
+    .sum()
+}
+
+pub fn p2(input: &[&str]) -> u32 {
+  input
+    .iter()
+    .map(|line| line.as_bytes())
+    .array_chunks::<3>()
+    .map(|chunk| item_priority(common(&chunk)))
+    .sum()
+}
+
+#[cfg(test)]
+mod tests {
+  use rstest::rstest;
+
+  use super::*;
+
+  #[rstest]
+  #[case(
+    "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw",
+    157
+  )]
+  fn test_p1(#[case] input: &str, #[case] expected: u32) {
+    assert_eq!(p1(&parse(input)), expected, "input: {input}");
+  }
+
+  #[rstest]
+  #[case(
+    "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw",
+    70
+  )]
+  fn test_p2(#[case] input: &str, #[case] expected: u32) {
+    assert_eq!(p2(&parse(input)), expected, "input: {input}");
+  }
+}
