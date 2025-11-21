90963094af8c17dea28330e7cd58a556875ee580
Kelvin DeCosta <decostakelvin@gmail.com>
2025-11-22 00:33:23 +0300

    feat(`y2016.d05`): solve puzzle

--- src/y2016/README.md
+++ src/y2016/README.md
@@ -1,8 +1,9 @@
 # Advent of Code [2016](https://adventofcode.com/2016)
 
-|      Day       | Challenge                                                         | Benchmark (µs) |
-| :------------: | :---------------------------------------------------------------- | -------------: |
-| [01](./d01.rs) | [No Time for a Taxicab](https://adventofcode.com/2016/day/1)      |          21.51 |
-| [02](./d02.rs) | [Bathroom Security](https://adventofcode.com/2016/day/2)          |           17.8 |
-| [03](./d03.rs) | [Squares With Three Sides](https://adventofcode.com/2016/day/3)   |          21.55 |
-| [04](./d04.rs) | [Security Through Obscurity](https://adventofcode.com/2016/day/4) |         106.35 |
+|      Day       | Challenge                                                              | Benchmark (µs) |
+| :------------: | :--------------------------------------------------------------------- | -------------: |
+| [01](./d01.rs) | [No Time for a Taxicab](https://adventofcode.com/2016/day/1)           |          21.51 |
+| [02](./d02.rs) | [Bathroom Security](https://adventofcode.com/2016/day/2)               |           17.8 |
+| [03](./d03.rs) | [Squares With Three Sides](https://adventofcode.com/2016/day/3)        |          21.55 |
+| [04](./d04.rs) | [Security Through Obscurity](https://adventofcode.com/2016/day/4)      |         106.35 |
+| [05](./d05.rs) | [How About a Nice Game of Chess?](https://adventofcode.com/2016/day/5) |     224,192.89 |
---
+++ src/y2016/d05.rs
@@ -0,0 +1,150 @@
+//! # How About a Nice Game of Chess?
+//!
+//! The core logic of iterating through the numbers to find the right hash is
+//! the exact same as the puzzles on [`2015/04`].
+//!
+//! [`2015/04`]: crate::y2015::d04
+
+use std::sync::{
+  atomic::{AtomicBool, AtomicU32, Ordering},
+  Mutex,
+};
+
+use md5::{Digest, Md5};
+
+use crate::util::thread::spawn;
+
+pub struct PasswordSeed {
+  instructions: Vec<(u32, u32)>,
+  mask: u16,
+}
+
+pub struct State {
+  secret: String,
+  counter: AtomicU32,
+  password_seed: Mutex<PasswordSeed>,
+  done: AtomicBool,
+}
+
+/// Calculate the MD5 hash of the buffer.
+fn hash(buffer: &[u8], size: usize) -> [u8; 16] {
+  let mut hasher = Md5::new();
+  hasher.update(&buffer[..size]);
+  hasher.finalize().as_slice().try_into().unwrap()
+}
+
+/// Concatenate the secret and the number, and return the result as a buffer.
+fn build_hash_input(secret: &str, n: u32) -> ([u8; 64], usize) {
+  let string = format!("{secret}{n}");
+  let size = string.len();
+
+  let mut buffer = [0; 64];
+  buffer[0..size].copy_from_slice(string.as_bytes());
+
+  (buffer, size)
+}
+
+/// Check if the hash of the buffer has five or six leading zeros.
+fn check_hash(buffer: &[u8], size: usize, n: u32, state: &State) {
+  let digest = hash(buffer, size);
+
+  // Since we only check for the first 5-6 leading zeros, we can use the
+  // just the first 4 bytes of the digest.
+  let target = u32::from_be_bytes(*digest.first_chunk::<4>().unwrap());
+
+  // To check for the number of leading zeros, we can use a bitmask.
+  if target & 0xffff_f000 == 0 {
+    let mut password_seed = state.password_seed.lock().unwrap();
+
+    password_seed.instructions.push((n, target));
+    password_seed.mask |= 1 << (target >> 8);
+
+    if password_seed.mask & 0xff == 0xff {
+      state.done.store(true, Ordering::Relaxed);
+    }
+  }
+}
+
+/// Checks the hash produced by combining the secret with 1000 numbers.
+#[allow(clippy::cast_possible_truncation)]
+fn worker(state: &State) {
+  while !state.done.load(Ordering::Relaxed) {
+    let offset = state.counter.fetch_add(1000, Ordering::Relaxed);
+    let (mut buffer, size) = build_hash_input(&state.secret, offset);
+
+    for n in 0..1000 {
+      // Update the hundreds, tens, and ones place of the number.
+      buffer[size - 3] = b'0' + (n / 100) as u8;
+      buffer[size - 2] = b'0' + ((n / 10) % 10) as u8;
+      buffer[size - 1] = b'0' + (n % 10) as u8;
+
+      check_hash(&buffer, size, offset + n, state);
+    }
+  }
+}
+
+pub fn parse(input: &str) -> Vec<u32> {
+  let state = State {
+    secret: input.trim().to_owned(),
+    counter: AtomicU32::new(1000),
+    password_seed: Mutex::new(PasswordSeed {
+      instructions: vec![],
+      mask: 0,
+    }),
+    done: AtomicBool::new(false),
+  };
+
+  // Check the first 1000 hashes sequentially.
+  for n in 1..1000 {
+    let (buffer, size) = build_hash_input(&state.secret, n);
+    check_hash(&buffer, size, n, &state);
+  }
+
+  spawn(|| worker(&state));
+
+  let mut password_seed =
+    state.password_seed.into_inner().unwrap().instructions;
+  password_seed.sort_unstable();
+  password_seed.iter().map(|&(_, n)| n).collect()
+}
+
+pub fn p1(input: &[u32]) -> String {
+  let password = input.iter().take(8).fold(0, |acc, n| (acc << 4) | (n >> 8));
+  format!("{password:08x}")
+}
+
+pub fn p2(input: &[u32]) -> String {
+  let mut password = 0;
+  let mut mask = 0xffff_ffff;
+
+  for n in input {
+    let sixth = n >> 8;
+    if sixth < 8 {
+      let shift = 4 * (7 - sixth);
+      let seventh = (n >> 4) & 0xf;
+      password |= (seventh << shift) & mask;
+      mask &= !(0xf << shift);
+    }
+  }
+
+  format!("{password:08x}")
+}
+
+#[cfg(test)]
+mod tests {
+  use rstest::rstest;
+
+  use super::*;
+
+  #[rstest]
+  #[case("abc", "18f47a30")]
+  fn test_p1(#[case] input: &str, #[case] expected: &str) {
+    assert_eq!(p1(&parse(input)), expected, "input: {input}");
+  }
+
+  #[rstest]
+  #[case("abc", "05ace8e3")]
+  fn test_p2(#[case] input: &str, #[case] expected: &str) {
+    assert_eq!(p2(&parse(input)), expected, "input: {input}");
+  }
+}
